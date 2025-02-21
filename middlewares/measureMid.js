58e450e822ade429e878c05950baff68d559a5fd
Shmuel atar <shmuelatar@gmail.com>
2025-02-21 22:55:26 +0200

    Create measurement implemented the feature of creating new measure to specific user using his id

--- middlewares/measureMid.js
+++ middlewares/measureMid.js
@@ -1,9 +1,25 @@
 const {pool} = require('../db/dbConnection')
 
 async function createMeasure(req,res,next) {
-    // userId,syst,dias,pulse   
-    console.log("createMeasure");
-    
+
+      
+    try {   
+        if(!req.params.userId) throw new Error("ID required!.")
+        if(!req.body.syst || isNaN(+req.body.syst) || +req.body.syst <= 0 ) throw new Error("Systolic must be provided and needs to be a positive number")
+        if(!req.body.dias || isNaN(+req.body.dias) || +req.body.dias <= 0 ) throw new Error("Diastolic must be provided and needs to be a positive number")
+        if(!req.body.pulse || isNaN(+req.body.pulse) || +req.body.pulse <= 0 ) throw new Error("Pulse must be provided and needs to be a positive number")
+
+
+        let sqlQuery ="insert into measures (user_id,date,syst_high,dias_low,pulse) Values (?,?,?,?,?)";
+        let queryValues = [+req.params.userId,new Date().toISOString().split('T')[0],+req.body.syst,+req.body.dias,+req.body.pulse]
+        const [data] = await pool.query(sqlQuery,queryValues)
+        console.log(data);
+
+        req.measureId = data.insertId;
+        next()
+    } catch (error) {
+        res.status(400).json({ message: `${error.sqlMessage || error.message}` })
+    }   
 }
 
 async function getMeasure(req,res,next) {
@@ -12,7 +28,7 @@ async function getMeasure(req,res,next) {
 
 }
 
-async function getAllMeasures(req,res,next) {
+async function getAllMeasuresAvg(req,res,next) {
     //(startDate,endDate)
     console.log("getAllMeasures");
 
--- router/measureRouter.js
+++ router/measureRouter.js
@@ -1,21 +1,21 @@
 const { Router } = require("express")
-const  {createMeasure,getMeasure,getAllMeasures,updateMeasure,deleteMeasure} = require("../middlewares/measureMid")
+const  {createMeasure,getMeasure,getAllMeasuresAvg,updateMeasure,deleteMeasure} = require("../middlewares/measureMid")
 const measureRouter = Router()
 
-measureRouter.get('/:id',getMeasure,(req,res)=>{
-    res.status(200).json({message:"OK"})
+// measureRouter.get('/:userId',getMeasure,(req,res)=>{
+//     res.status(200).json({message:"OK"})
+// })
+// measureRouter.get('/',getAllMeasuresAvg,(req,res)=>{
+//     res.status(200).json({message:"OK"})
+// })
+measureRouter.post('/:userId',createMeasure,(req,res)=>{
+    res.status(200).json({message:`Measure has been created successfully, Measure ID: ${req.measureId}`})
 })
-measureRouter.get('/',getAllMeasures,(req,res)=>{
-    res.status(200).json({message:"OK"})
-})
-measureRouter.post('/',createMeasure,(req,res)=>{
-    res.status(200).json({message:"OK"})
-})
-measureRouter.patch('/:id',updateMeasure,(req,res)=>{
-    res.status(200).json({message:"OK"})
-})
-measureRouter.delete('/:id',deleteMeasure,(req,res)=>{
-    res.status(200).json({message:"OK"})
-})
+// measureRouter.patch('/:id',updateMeasure,(req,res)=>{
+//     res.status(200).json({message:"OK"})
+// })
+// measureRouter.delete('/:id',deleteMeasure,(req,res)=>{
+//     res.status(200).json({message:"OK"})
+// })
 
 module.exports = { measureRouter }
