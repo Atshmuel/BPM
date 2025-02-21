0552421e33befd63c548543b8016802e9f23bd6d
Shmuel atar <shmuelatar@gmail.com>
2025-02-21 16:08:37 +0200

    Two new middlewares for user router Created update and get all users mids to allow the user update name and to get all the users list from the DB

--- middlewares/userMid.js
+++ middlewares/userMid.js
@@ -3,9 +3,9 @@ const {pool} = require('../db/dbConnection')
 
 async function createUser(req,res,next){
     try {
-        if(!req.body.fullName || req.body.fullName.split(' ').length < 2)   throw new Error("Name is not allowed, you must provide name with minimun of Two letters.")
+        if(!req.body.fullName || req.body.fullName.split(' ').length != 2)   throw new Error("Name is not allowed to be empty, you must provide name with minimun of Two letters, make sure to provide full name (two words only).")
         let sqlQuery ="insert into users (full_name) values (?)";
-        let queryValues = [req.body.fullName]
+        let queryValues = [req.body.fullName.trim()]
         const [data] = await pool.query(sqlQuery,queryValues)
         
         req.userId = data.insertId;
@@ -31,14 +31,31 @@ async function getUser(req,res,next){
 }
 
 async function getAllUsers(req,res,next){
-    //() 
-console.log("getAllUsers");
-    
+    try {        
+        let sqlQuery ="select * from users";
+        const [data] = await pool.query(sqlQuery)
+        req.allUsers = data;
+        next()
+    } catch (error) {
+        res.status(400).json({ message: `${error.sqlMessage || error.message}` })
+    }    
 }
 
 async function updateUser(req,res,next){
-    //(id,newName) 
-console.log("updateUser");
+    try {   
+        if(!req.params.id) throw new Error("ID required!.")
+            if(!req.body.fullName || req.body.fullName.split(' ').length != 2)   throw new Error("Name is not allowed to be empty, you must provide name with minimun of Two letters, make sure to provide full name (two words only).")
+        let sqlQuery ="update users set full_name = (?) where id = (?)";
+        let queryValues = [req.body.fullName.trim(),req.params.id]
+        const [data] = await pool.query(sqlQuery,queryValues)
+        console.log(data);
+        if(!data.affectedRows) throw new Error("ID could not be found in the DB.")
+        if(data.affectedRows &&  !data.changedRows) throw new Error("No change detected, please provide new name to make the change.")
+        req.changes = data.changedRows;
+        next()
+    } catch (error) {
+        res.status(400).json({ message: `${error.sqlMessage || error.message}` })
+    }   
     
 }
 
--- router/userRouter.js
+++ router/userRouter.js
@@ -3,16 +3,16 @@ const {createUser,getAllUsers,getUser,updateUser,deleteUser} = require("../middl
 const userRouter = Router()
 
 userRouter.get('/:id',getUser,(req,res)=>{
-    res.status(200).json({message:"OK"})
+    res.status(200).json({message:"OK",data:req.userData})
 })
 userRouter.get('/all',getAllUsers,(req,res)=>{
-    res.status(200).json({message:"OK"})
+    res.status(200).json({message:"OK",data:req.allUsers})
 })
 userRouter.post('/',createUser,(req,res)=>{
     res.status(200).json({message:`${req.body.fullName}'s has been created and stored in rowID: ${req.userId}.`})
 })
 userRouter.patch('/:id',updateUser,(req,res)=>{
-    res.status(200).json({message:"OK"})
+    res.status(200).json({message:"Changed successfully."})
 })
 userRouter.delete('/:id',deleteUser,(req,res)=>{
     res.status(200).json({message:"OK"})
